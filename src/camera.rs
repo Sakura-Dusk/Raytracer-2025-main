3cb3859bf7fdf88567a8f125c6c3407bff1e1d14
Sakura_Dusk <sakura_dusk@sjtu.edu.cn>
2025-06-26 14:54:06 +0800

    Finish book1 Image9

--- src/camera.rs
+++ src/camera.rs
@@ -111,7 +111,7 @@ impl Camera {
         }
 
         let mut rec: hittable::HitRecord = hittable::HitRecord::new();
-        if world.hit(&r, &Interval::new(0.0, f64::INFINITY), &mut rec) {
+        if world.hit(&r, &Interval::new(0.001, f64::INFINITY), &mut rec) {
             let direction = random_on_hemisphere(&rec.normal);
             return 0.5 * self.ray_color(&Ray::new(rec.p, direction), depth - 1, world);
         }
@@ -134,7 +134,7 @@ impl Camera {
     pub fn render(&mut self, world: &dyn Hittable) {
         self.initialize();
 
-        let path = std::path::Path::new("output/book1/image8.png");
+        let path = std::path::Path::new("output/book1/image9.png");
         let prefix = path.parent().unwrap();
         std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
 
