8d5789b97be299bbe7247b98732bda0619fb2dee
Sakura_Dusk <sakura_dusk@sjtu.edu.cn>
2025-06-26 15:24:30 +0800

    Finish book1 Image12

--- src/camera.rs
+++ src/camera.rs
@@ -134,7 +134,7 @@ impl Camera {
     pub fn render(&mut self, world: &dyn Hittable) {
         self.initialize();
 
-        let path = std::path::Path::new("output/book1/image11.png");
+        let path = std::path::Path::new("output/book1/image12.png");
         let prefix = path.parent().unwrap();
         std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
 
--- src/rtweekend/color.rs
+++ src/rtweekend/color.rs
@@ -4,9 +4,9 @@ use image::Rgb;
 
 pub(crate) type Color = Vec3;
 pub(crate) fn write_color(pixel: &mut Rgb<u8>, pixel_color: &Color) {
-    let r = pixel_color.x;
-    let g = pixel_color.y;
-    let b = pixel_color.z;
+    let r = linear_to_gamma(pixel_color.x);
+    let g = linear_to_gamma(pixel_color.y);
+    let b = linear_to_gamma(pixel_color.z);
 
     //Translate the [0,1] component values to the byte range [0,255].
     let intensity: Interval = Interval::new(0.0, 0.999);
@@ -15,4 +15,11 @@ pub(crate) fn write_color(pixel: &mut Rgb<u8>, pixel_color: &Color) {
     let bbyte = (256.0 * intensity.clamp(b)) as u8;
 
     *pixel = Rgb([rbyte, gbyte, bbyte]);
+}
+
+pub fn linear_to_gamma(linear_component: f64) -> f64 {
+    if linear_component > 0.0 {
+        return linear_component.sqrt();
+    }
+    0.0
 }
