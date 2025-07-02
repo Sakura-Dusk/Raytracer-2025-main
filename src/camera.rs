2780718c9d1836095e4d4bdbab2a37d475120ed7
Sakura_Dusk <sakura_dusk@sjtu.edu.cn>
2025-07-02 13:07:55 +0800

    Rebuild book3 Image2

--- src/camera.rs
+++ src/camera.rs
@@ -177,7 +177,7 @@ impl Camera {
     pub fn render(&mut self, world: &dyn Hittable) {
         self.initialize();
 
-        let path = std::path::Path::new("output/book3/image3.png");
+        let path = std::path::Path::new("output/book3/image2.png");
         let prefix = path.parent().unwrap();
         std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
 
